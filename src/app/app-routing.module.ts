f519f5c57b61aebe4814ef7a3f8deac598104404
Federico Fabre <fedefabre@MacBook-Pro-de-Federico.local>
2019-03-13 10:33:27 -0300

    rounting fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
+  { path: '', component: HomeComponent },
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
